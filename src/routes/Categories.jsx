3c6c3f5549b7b2fa0bc676c9b8705280bffe8dcb
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-09-09 22:10:44 -0600

    fetching categories

--- src/routes/Categories.jsx
+++ src/routes/Categories.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Categories() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pulpe-admin-api.herokuapp.com/categories/", {
+      method: "GET",
+    }).then((response) =>
+      response.json().then((json) => {
+        setCategories(json.data);
+      })
+    );
+  }, []);
+
   return (
-    <>
-      <h1>Categories</h1>
-    </>
+    <div className="container">
+      <div className="row m-3">
+        <div className="col d-flex justify-content-center">
+          <h1>Categories</h1>
+        </div>
+      </div>
+      <div className="row row-cols-1 mt-2 mb-2">
+        {categories.length ? (
+          categories.map((category) => {
+            return (
+              <>
+                <div className="col mt-2 mb-2">
+                  <div className="card">
+                    <div className="card-body">
+                      <h5 className="card-title">{category.name}</h5>
+                      <h6 className="card-subtitle mb-2 text-muted">
+                        {category.description || ""}
+                      </h6>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
   );
 }
 
