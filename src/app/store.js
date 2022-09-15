cf84ebe4eee03ac6064471fe769fcefe3a078414
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-09-15 04:31:16 -0600

    adding a redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,15 @@
+// By now you might be wondering, "Do I always have to put all my app's state into the Redux store?"
+
+// The answer is NO. Global state that is needed across the app should go in the Redux store.
+// State that's only needed in one place should be kept in component state.
+
 import { configureStore } from "@reduxjs/toolkit";
+import categoriesReducer from "../features/categories/categoriesSlice";
+import productsReducer from "../features/products/productsSlice";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    categories: categoriesReducer,
+    products: productsReducer,
+  },
 });
