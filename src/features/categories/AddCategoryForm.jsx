40b7f38203f780af86536e47dffb7a3ff905e2d8
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-09-15 04:35:27 -0600

    adding create category and create product forms

---
+++ src/features/categories/AddCategoryForm.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+import { categoryAdded } from "./categoriesSlice";
+
+function AddCategoryForm() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const onNameChanged = (e) => setName(e.target.value);
+  const onDescriptionChanged = (e) => setDescription(e.target.value);
+
+  const onModalClose = () => navigate(-1);
+  const onModalSaveChanges = () => {
+    if (name && description) {
+      dispatch(
+        categoryAdded({
+          id: nanoid(),
+          name,
+          description,
+        })
+      );
+    }
+    navigate(-1);
+  };
+
+  return (
+    <section>
+      <Modal
+        show={true}
+        onHide={onModalClose}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add a new category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <div className="mb-3">
+              <label htmlFor="categoryName" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="categoryName"
+                value={name}
+                onChange={onNameChanged}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="categoryDescription" className="form-label">
+                Description
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="categoryDescription"
+                value={description}
+                onChange={onDescriptionChanged}
+              />
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={onModalClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={onModalSaveChanges}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </section>
+  );
+}
+
+export default AddCategoryForm;
---
+++ src/features/products/AddProductForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddProductForm() {
+  return <></>;
+}
+
+export default AddProductForm;
