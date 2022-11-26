2047e342e9b2aad43a3040ff052d32709dbbb594
Axel Somerseth Cordova <axelsomerseth@gmail.com>
2022-11-25 21:35:55 -0600

    Footer component migrated to react bootstrap

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Footer() {
   return (
     <footer>
-      <nav className="navbar fixed-bottom bg-light">
-        <div className="container-fluid text-center">
-          <div className="col-12">
-            <span className="text-muted">
-              Made with love remotely &#x1FAF6;
-            </span>
-          </div>
-        </div>
-      </nav>
+      <Navbar fixed="bottom" bg="light">
+        <Container fluid className="justify-content-md-center">
+          <Row>
+            <Col>
+              <span className="text-muted">
+                Made with love remotely &#x1FAF6;
+              </span>
+            </Col>
+          </Row>
+        </Container>
+      </Navbar>
     </footer>
   );
 }
