0200550f995c926143c88c720fff0bad67107c50
Axel Somerseth Cordova <axelsomerseth@gmail.com>
2022-11-25 22:29:17 -0600

    Footer to sfc

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-function Footer() {
+const Footer = () => {
   return (
     <footer>
       <Navbar fixed="bottom" bg="light">
@@ -20,6 +20,6 @@ function Footer() {
       </Navbar>
     </footer>
   );
-}
+};
 
 export default Footer;
