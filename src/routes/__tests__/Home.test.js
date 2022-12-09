eeb43affe66f0d4b5961c84dba1cacbbeeb644ec
Axel Somerseth Cordova <axelsomerseth@gmail.com>
2022-12-09 02:05:57 -0600

    home comp test

--- src/routes/__tests__/Home.test.js
+++ src/routes/__tests__/Home.test.js
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../Home";
 
-// * Docs: https://testing-library.com/docs/example-react-router/
 it("renders Home component", () => {
   render(<Home />);
   const h1Element = screen.getByText(/Home/i);
