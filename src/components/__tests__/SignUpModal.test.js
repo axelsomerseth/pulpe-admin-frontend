4625e13319f189ba8359e24986d62a2447b10e94
Axel Somerseth Cordova <axelsomerseth@gmail.com>
2022-12-12 13:26:09 -0600

    testing SignUpModal close button

--- src/components/__tests__/SignUpModal.test.js
+++ src/components/__tests__/SignUpModal.test.js
@@ -1,13 +1,15 @@
-import { screen } from "@testing-library/react";
+import { screen, fireEvent } from "@testing-library/react";
 import { render } from "../../utils/customRender";
 import SignUpModal from "../SignUpModal";
 
 it("renders SignUpModal component", () => {
   // arrange
-  render(<SignUpModal show={true} />);
+  render(<SignUpModal show={true} setShow={() => {}} />);
 
   // act
   const modalTitle = screen.getByText(/Create an account/i);
+  const closeButton = screen.getByText(/Close/i);
+  fireEvent.click(closeButton);
 
   // assert
   expect(modalTitle).toBeInTheDocument();
