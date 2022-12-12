e46c42b79e5183bfdbeb54e20a653cf32722f7d4
Axel Somerseth Cordova <axelsomerseth@gmail.com>
2022-12-12 15:15:49 -0600

    configure redux store at customRender for tests

--- src/utils/customRender.jsx
+++ src/utils/customRender.jsx
@@ -1,21 +1,61 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { UserContext } from "../App";
-import { BrowserRouter } from "react-router-dom";
-import store from "../app/store";
+
 import { Provider as ReduxProvider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import categoriesReducer from "../features/categories/categoriesSlice";
+import productsReducer from "../features/products/productsSlice";
+import transactionsReducer from "../features/transactions/transactionsSlice";
+
+const setUpStore = () => {
+  const preloadedState = {
+    categories: {
+      categories: [
+        {
+          id: 1,
+          name: "Test name",
+          description: "Test description",
+          createdAt: new Date(),
+        },
+      ],
+      status: "idle",
+      error: null,
+    },
+    products: {
+      products: [],
+      status: "idle",
+      error: null,
+    },
+    transactions: {
+      transactions: [],
+      status: "idle",
+      error: null,
+    },
+  };
+  const store = configureStore({
+    reducer: {
+      categories: categoriesReducer,
+      products: productsReducer,
+      transactions: transactionsReducer,
+    },
+    preloadedState,
+  });
+
+  return store;
+};
 
 const AllTheProviders = ({ children }) => {
   const user = { username: "test@example.mock", id: 1 };
   const setUser = () => {};
 
+  const store = setUpStore();
+
   return (
     <ReduxProvider store={store}>
-      <BrowserRouter>
-        <UserContext.Provider value={{ user, setUser }}>
-          {children}
-        </UserContext.Provider>
-      </BrowserRouter>
+      <UserContext.Provider value={{ user, setUser }}>
+        {children}
+      </UserContext.Provider>
     </ReduxProvider>
   );
 };
