2eb284a0ea88844a205e9d2c3ac07320072dd0f5
Axel Somerseth Cordova <axelsomerseth@gmail.com>
2022-12-12 16:13:09 -0600

    added one test todo

---
+++ src/features/transactions/__tests__/SingleTransactionPage.test.js
@@ -0,0 +1,10 @@
+// import { render } from "../../../utils/customRender";
+// import { screen, fireEvent } from "@testing-library/react";
+// import { MemoryRouter, Routes, Route } from "react-router-dom";
+// import SingleTransactionPage from "../SingleTransactionPage";
+
+describe("renders SingleTransactionPage component", () => {
+  it.todo("with data");
+
+  it.todo("without data");
+});
